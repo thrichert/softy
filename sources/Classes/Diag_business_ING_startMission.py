ddf1e24175963195a773298e52bbe4a1d9e2cdf0
thomas <thomas.richert@eikosim.com>
2021-01-11 01:03:59 +0100

    remove direct access to DB : use ING Class instead - add function of automatic set previous missions in archive if stop date < current date

--- sources/Classes/Diag_business_ING_startMission.py
+++ sources/Classes/Diag_business_ING_startMission.py
@@ -10,19 +10,25 @@ class Diag_business_ING_startMission(QtWidgets.QDialog):
 	def __init__(self, viewPath, selectedIng, database):
 		super(Diag_business_ING_startMission, self).__init__()
 		uic.loadUi(viewPath, self)
-		content = database.getContent()
 		# modify group box name
 		self.groupboxName = self.findChild(QtWidgets.QGroupBox, "Ing_groupBox")
 		self.groupboxName.setTitle(selectedIng)
+
+		missionStartDate = self.findChild(QtWidgets.QDateEdit, "startDate")
+		missionStopDate = self.findChild(QtWidgets.QDateEdit, "stopDate")
+		clientName = self.findChild(QtWidgets.QLineEdit, "clientName")
+
+		missionStartDate.setDate(QtCore.QDate.currentDate())
+		missionStopDate.setDate(QtCore.QDate.currentDate().addMonths(3))
+
 		resp = self.exec_()
 		if resp == QtWidgets.QDialog.Accepted:
-			missionStartDate = self.findChild(QtWidgets.QDateEdit, "startDate")
-			missionStopDate = self.findChild(QtWidgets.QDateEdit, "stopDate")
-			clientName = self.findChild(QtWidgets.QLineEdit, "clientName")
+			ing = ING.load(database, selectedIng)
 			#check user input
 			clientNameText = clientName.text()
 			clientNameCheck = False
 			dateCheck = False
+			isArchived = False
 			if (clientNameText == ""):
 				alert = QtWidgets.QMessageBox()
 				alert.setText('error - Client Name cannot be empty')
@@ -33,16 +39,24 @@ class Diag_business_ING_startMission(QtWidgets.QDialog):
 				alert = QtWidgets.QMessageBox()
 				alert.setText('error - Stop mission happens before Start mission')
 				alert.exec_()
+			elif missionStopDate.date() < QtCore.QDate.currentDate():
+				alert = QtWidgets.QMessageBox()
+				alert.setText('Warning - Mission stop already passed -\nMission registered has archived\n{name} state stays {state}'.format(name=selectedIng, state=ing.getState()))
+				alert.exec()
+				isArchived = True
+				prevState = ing.getStateKey()
 			else:
 				dateCheck = True
 
 			# add info to BD
-			if clientNameCheck and dateCheck:
-				ingID = ING.getIngIDfromName(selectedIng, database)
-				content["INGs"][ingID]["current_client"] = clientNameText
-				content["INGs"][ingID]["mission_Start"] = missionStartDate.date().toString("dd.MM.yyyy")
-				content["INGs"][ingID]["mission_Stop"] = missionStopDate.date().toString("dd.MM.yyyy")
-				content["INGs"][ingID]["state"] = "with Mission"
-				database.write(content)
+			if clientNameCheck and (dateCheck or isArchived):
+				ing.setCurrentClient(clientNameText)
+				ing.setMissionStart(missionStartDate.date().toString("dd.MM.yyyy"))
+				ing.setMissionStop(missionStopDate.date().toString("dd.MM.yyyy"))
+				ing.setState(ING.ING_STATE_MI)
+				ing.save()
+				if (isArchived):
+					ing.stopMission(prevState)
+					ing.save()
 		else:
 			print ("Nop")
