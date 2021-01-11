89034b07c12987a271e2939fd53c59ee7d226556
thomas <thomas.richert@eikosim.com>
2021-01-11 01:10:12 +0100

    Dialog work now for ING and IA

--- sources/Classes/Diag_delete_ing_ia.py
+++ sources/Classes/Diag_delete_ing_ia.py
@@ -2,6 +2,8 @@ from PyQt5 import QtWidgets, uic, QtCore, QtGui
 import sys, os, json
 from Classes.DB import DB
 from Classes.ING import ING
+from Classes.IngAffaire import IngAffaire
+from Classes.User import User
 
 
 class Diag_delete_ing_ia(QtWidgets.QDialog):
@@ -9,13 +11,14 @@ class Diag_delete_ing_ia(QtWidgets.QDialog):
 	Class that handle a dialog window to delete an Engineer or an BusinessIng
 	"""
 
-	def __init__(self, viewPath, selectedIng, database):
+	def __init__(self, viewPath, userType, selectedUsr, database):
 		super(Diag_delete_ing_ia, self).__init__()
 		uic.loadUi(viewPath, self)
-		content = database.getContent()
+		self.deleted = False
+
 		# modify group box name
 		self.groupboxName = self.findChild(QtWidgets.QGroupBox, "Ing_groupBox")
-		self.groupboxName.setTitle(selectedIng)
+		self.groupboxName.setTitle(selectedUsr)
 
 		self.endContractDateEdit = self.findChild(QtWidgets.QDateEdit,	"contract_end_date")
 		self.endContractDateEdit.setDate(QtCore.QDate.currentDate())
@@ -25,34 +28,33 @@ class Diag_delete_ing_ia(QtWidgets.QDialog):
 		resp = self.exec_()
 
 		if resp == QtWidgets.QDialog.Accepted:
+			if userType == User._ING:
+				usr = ING.load(database, selectedUsr)
+			elif userType == User._IA:
+				usr = IngAffaire.load(database, selectedUsr)
+			else:
+				usr = None
+				print ("error - {} not found".format(selectedUsr))
+				return
+			ingEntryDate = usr.getEntryDate()
 
-			ingID = ING.getIngIDfromName(selectedIng, database)
-			ingEntryDate = QtCore.QDate().fromString(content["INGs"][ingID]['entryDate'], 'dd.MM.yyyy')
-
+			self.motifText = self.motifEndContract.text()
+			self.exitDateText = self.endContractDateEdit.date().toString('dd.MM.yyyy')
 			motifCheck = False
 			dateCheck = False
 			#check user Input
-			if self.motifEndContract.text() == "":
+			if self.motifText == "":
 				alert = QtWidgets.QMessageBox()
 				alert.setText("error - Motif cannot be empty")
 				alert.exec_()
 			else:
 				motifCheck = True
-			if self.endContractDateEdit.date() < ingEntryDate:
+			if self.endContractDateEdit.date() < QtCore.QDate.fromString(ingEntryDate,'dd.MM.yyyy') :
 				alert = QtWidgets.QMessageBox()
 				alert.setText('error - end contract happens before entry date')
 				alert.exec_()
 			else:
 				dateCheck = True
-
-			if dateCheck and motifCheck:
-				ingData = content["INGs"][ingID]
-				# remove ing from Ings
-				content["INGs"].pop(ingID)
-				# add data to Archives
-				content["archive"]["INGs"][ingID] = ingData
-				content["archive"]["INGs"][ingID]["endContract"] = self.endContractDateEdit.date().toString('dd.MM.yyyy')
-				content["archive"]["INGs"][ingID]["motif"] = self.motifEndContract.text()
-
-				#save db
-				database.write(content)
+			self.deleted = (dateCheck and motifCheck)
+			if self.deleted:
+				usr.delete(self.exitDateText, self.motifText)
